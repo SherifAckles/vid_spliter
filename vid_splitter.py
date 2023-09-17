a470941645e4b5bea93ecc344622bc4350e0a429
nabesteron <49988463+nabesteron@users.noreply.github.com>
2023-09-17 10:56:08 -0500

    define the output dir

--- vid_splitter.py
+++ vid_splitter.py
@@ -1,5 +1,8 @@
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 
 #let's define the input and output file directory
-
+#input vid
 input_vid="input_vid.mp4"
+
+#output dir
+output_dir="output_segments/"
