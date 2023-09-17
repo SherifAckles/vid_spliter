0b4af0c9dd5b1dc931557202f094f705124f937a
nabesteron <49988463+nabesteron@users.noreply.github.com>
2023-09-17 11:04:21 -0500

    calc end time of the current seg

--- vid_splitter.py
+++ vid_splitter.py
@@ -1,22 +1,28 @@
+from moviepy.video.io.VideoFileClip import VideoFileClip
+import os
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 
-#let's define the input and output file directory
-#input vid
-input_vid="input_vid.mp4"
+# let's define the input and output file directory
+# input vid
+input_vid = "input_vid.mp4"
 
-#output dir
-output_dir="output_segments/"
+# output dir
+output_dir = "output_segments/"
 
 # Create the output directory if it doesn't exist
-import os
 os.makedirs(output_dir, exist_ok=True)
 
 # Calculate the total duration of the video in seconds
-from moviepy.video.io.VideoFileClip import VideoFileClip
 video = VideoFileClip(input_vid)
 total_duration = video.duration
 
 # Split the video into 30-second segments
 segment_duration = 30  # in seconds
 
+# Define the range for splitting the video based on segment duration
+for start_time in range(0, int(total_duration), segment_duration):
 
+    # Calculate the end time of the current segment
+    end_time = start_time + segment_duration
+
+   
